e716795117e5d073167a00e0d50b129695de30e1
Marcos <marcosaraujo.f@hotmail.com>
2021-01-09 14:36:29 -0300

    Add Footer

---
+++ sds2-aulas/front-web/src/Footer/index.tsx
@@ -0,0 +1,24 @@
+import './styles.css';
+import {ReactComponent as YouTubeIcon} from './youtube.svg';
+import {ReactComponent as LinkedinIcon} from './linkedin.svg';
+import {ReactComponent as InstagramIcon} from './instagram.svg';
+
+function Footer(){
+   return (
+       <footer className="main-footer">
+          App desenvolvido durante a 2ª ed. do envento Semana DevSuperior 
+          <div className="footer-icons">
+              <a href="https://www.youtube.com.br/" target="_new">
+                <YouTubeIcon />
+              </a>
+              <a href="https://www.linkedin.com/" target="_new">
+                <LinkedinIcon />
+              </a>
+              <a href="https://www.instagram.com/" target="_new">
+                <InstagramIcon />
+              </a>
+          </div>
+       </footer>
+   ) 
+}
+export default Footer;
