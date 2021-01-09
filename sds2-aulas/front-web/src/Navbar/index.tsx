9d083a15b12d290634b722fdd4e61ea73aa360c9
Marcos <marcosaraujo.f@hotmail.com>
2021-01-09 16:24:06 -0300

    Add routes and StepsHeader

--- sds2-aulas/front-web/src/Navbar/index.tsx
+++ sds2-aulas/front-web/src/Navbar/index.tsx
@@ -1,11 +1,12 @@
 import './styles.css';
 import {ReactComponent as Logo} from './logo.svg';
+import { Link } from 'react-router-dom';
 
 function Navbar(){
    return (
        <nav className="main-navbar">
            <Logo />
-           <a href="home" className="logo-test">DS Delivery</a>
+           <Link to="/" className="logo-test">DS Delivery</Link>
        </nav>
    ) 
 }
