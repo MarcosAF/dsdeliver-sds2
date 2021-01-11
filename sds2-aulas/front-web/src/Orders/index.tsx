dc1bd136ac98b4b80db2cb92c8dde6a785413173
Marcos <marcosaraujo.f@hotmail.com>
2021-01-10 22:57:43 -0300

    Finish front-web

--- sds2-aulas/front-web/src/Orders/index.tsx
+++ sds2-aulas/front-web/src/Orders/index.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
-import { fetchProducts } from '../api';
+import { toast} from 'react-toastify';
+import { fetchProducts, saveOrder } from '../api';
+import Footer from '../Footer';
+import { checkIsSelected } from './helpers';
 import OrdersLocation from './OrderLocation';
+import OrderSummary from './OrderSummary';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
@@ -8,7 +12,11 @@ import { OrderLocationdata, Product } from './types';
 
 function Orders(){
    const [products, setProducts] = useState<Product[]>([]);
+   const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+   const totalPrice = selectedProducts.reduce((sum, item) => {
+      return sum + item.price
+   },0);
       
    useEffect(() => {
       fetchProducts()
@@ -16,13 +24,52 @@ function Orders(){
          .catch(error => console.log(error))
    }, []);
 
+   const handleSelectProduct = (product: Product) => {
+      const isAlreadySelected = checkIsSelected(selectedProducts, product);
+    
+      if (isAlreadySelected) {
+        const selected = selectedProducts.filter(item => item.id !== product.id);
+        setSelectedProducts(selected);
+      } else {
+        setSelectedProducts(previous => [...previous, product]);
+      }
+    }
+    const handleSubmit = () => {
+      const productsIds = selectedProducts.map(({ id }) => ({ id }));
+      const payload = {
+        ...orderLocation!,
+        products: productsIds
+      }
+    
+      saveOrder(payload).then((response) => {
+        toast.error(`Pedido enviado com sucesso! Nº ${response.data.id}`);
+        setSelectedProducts([]);
+      })
+        .catch(() => {
+          toast.warning('Erro ao enviar pedido');
+        })
+    }
 
    return (
+      <>
       <div className="orders-container">
          <StepsHeader />
-         <ProductsList products={products} />
+         <ProductsList 
+         products={products} 
+         onSelectProduct={handleSelectProduct}
+         selectedProducts={selectedProducts}
+         />
          <OrdersLocation onChangeLocation={location => setOrderLocation(location)} />
+
+         <OrderSummary 
+         amount={selectedProducts.length} 
+         totalPrice={totalPrice} 
+         onSubmit={handleSubmit}
+         />
       </div>
+      <Footer />
+      </>
+     
    ) 
 }
 export default Orders;
--- sds2-aulas/front-web/src/Orders/types.ts
+++ sds2-aulas/front-web/src/Orders/types.ts
@@ -11,3 +11,10 @@ export type OrderLocationdata = {
     longitude: number;
     address: string;
 }
+type ProductId = {
+    id: number ;
+
+}
+export type OrderPayLoad = {
+    products: ProductId[];
+} & OrderLocationdata;
